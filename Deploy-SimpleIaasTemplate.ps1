f7ff075bcd7f434787a73d34caae18f2aff09d2c
Clayton Begg <clayton_begg@hotmail.com>
2018-11-18 15:54:11 +1100

    First Commit

---
+++ Deploy-SimpleIaasTemplate.ps1
@@ -0,0 +1,26 @@
+﻿### Define Deployment Variables
+{
+$location = 'Australia Southeast'
+$resourceGroupName = 'ElpherCode-iaas-template'
+$resourceDeploymentName = 'ElpherCode-iaas-template-deployment'
+$templatePath = 'D:\code\GitRepos\AzureRM'
+$templateFile = 'simpleiaas.json'
+$template = $templatePath + '\' + $templateFile
+}
+
+### Create Resource Group
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $location `
+    -Verbose -Force
+}
+
+### Deploy Resources
+{
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -Verbose -Force
+}
